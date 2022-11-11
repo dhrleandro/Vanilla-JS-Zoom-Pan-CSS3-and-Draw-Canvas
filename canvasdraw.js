11dabcf655ceb0ef359aa5932febd9bf1afb8d09
Leandro Daher <leandrodaher96@gmailcom>
2022-11-11 16:12:26 -0300

    Free hand draw

--- canvasdraw.js
+++ canvasdraw.js
@@ -5,26 +5,16 @@ export default class CanvasDraw {
     this.paintStart = false;
     this.canvas = document.getElementById(canvasId);
     this.ctx = canvas.getContext('2d');
+    this.lastPoint = {x: 0, y: 0};
 
     this.canvas.addEventListener('pointerdown', this.onpointerdown.bind(this));
     window.addEventListener('pointerup', this.onpointerup.bind(this));
     this.canvas.addEventListener('pointermove', this.onpointermove.bind(this));
   }
 
-  onpointerdown(event) {
-    const middleButton = (event.button === 1);
+  getOffsetPoint(event) {
+    const rect = this.canvas.getBoundingClientRect();
 
-    if (this.appState.tool === 'pen' && middleButton === false) {
-      this.paintStart = true;
-    }
-  }
-
-  onpointerup() {
-    this.paintStart = false;
-  }
-
-  onpointermove(event) {
-    const rect = canvas.getBoundingClientRect();
     const mouse = {
       x: event.clientX - rect.left,
       y: event.clientY - rect.top,
@@ -34,10 +24,36 @@ export default class CanvasDraw {
     mouse.x = mouse.x * canvas.width / rect.width;
     mouse.y = mouse.y * canvas.height / rect.height;
 
+    return mouse;
+  }
+
+  onpointerdown(event) {
+    const middleButton = (event.button === 1);
+
+    if (this.appState.tool === 'pen' && middleButton === false) {
+      this.paintStart = true;
+      this.lastPoint = this.getOffsetPoint(event);
+    }
+  }
+
+  onpointerup(event) {
+    this.paintStart = false;
+    this.lastPoint = this.getOffsetPoint(event);
+  }
+
+  onpointermove(event) {
+    const mouse = this.getOffsetPoint(event);
+
     if (this.appState.tool === 'pen' && this.paintStart) {
       this.ctx.fillStyle = 'yellow';
-      this.ctx.fillRect(mouse.x - 2, mouse.y - 2, 4, 4);
+      this.ctx.lineWidth = 8;
+      this.ctx.lineCap = 'round';
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.lastPoint.x, this.lastPoint.y);
+      this.ctx.lineTo(mouse.x, mouse.y);
+      this.ctx.stroke();
     }
+    this.lastPoint = mouse;
   }
 
   getCanvas() {
